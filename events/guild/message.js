cb304e2925e150ee5a075058fdf0d9368c23c9a2
Sterling Velazquez <SterlingVelazquez@users.noreply.github.com>
2022-01-05 16:12:42 -0500

    Added Scroll Keeper requirement

--- events/guild/message.js
+++ events/guild/message.js
@@ -3,6 +3,16 @@ const profileModel = require("../../models/profileSchema");
 
 module.exports = async (Discord, client, message) => {
     const prefix = process.env.PREFIX;
+    const admin = ["add", "edit", "delete"];
+
+    /* let role = message.guild.roles.cache.find(r => r.name === "Scroll Keeper");
+    console.log(role);
+    if (!role) message.guild.roles.create({
+        data: {
+            name: "#Scroll Keeper"
+        }
+    }) */
+
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     let profileData;
@@ -23,6 +33,9 @@ module.exports = async (Discord, client, message) => {
     const cmd = args.shift().toLowerCase();
 
     const command = client.commands.get(cmd);
+
+    if (!message.member.roles.cache.some(r => r.name == "Scroll Keeper") && command.name && admin.includes(command.name))
+        return message.channel.send(`Must have *Scroll Keeper* role to use '-${command.name}' command!`)
 
     if (command) command.execute(client, message, args, Discord, profileData);
 }
